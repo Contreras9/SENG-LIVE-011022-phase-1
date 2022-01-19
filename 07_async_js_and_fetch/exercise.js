da07376acc3a34f1eb147a90472abee885b537ed
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-01-19 15:43:19 -0800

    add solution to lesson 7 exercise on async js & fetch

--- 07_async_js_and_fetch/exercise.js
+++ 07_async_js_and_fetch/exercise.js
@@ -31,7 +31,7 @@ function renderTask(task) {
   dueDateEl.textContent = task.dueDate;
   completedEl.innerHTML = `<i class="far ${task.complete ? 'fa-check-square' : 'fa-square'} text-4xl text-green-300 cursor-pointer"></i>`;
   completedEl.addEventListener('click', (event) => {
-    toggleComplete(task.id);
+    toggleComplete(task);
   })
   document.querySelector('#todoList').append(li);
   return li;
@@ -59,9 +59,18 @@ function addTask(taskLabel, dueDate) {
   // 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 
   // BONUS Task 2: Add fetch request to persist the newTask to the json-server before adding it to the todoList and the DOM
   // 
+  fetch('http://localhost:3000/tasks', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(newTask)
+  })
+    .then(response => response.json())
+    .then(renderTask)
   // move the lines below inside of the promise callback so that you update the todoList and the DOM after the `newTask` is added to db.json via fetch.
   // You want to make sure that the newly added task has an id as that will be important later on.
-  renderTask(newTask)
+  // renderTask(newTask)
   return newTask
 }
 
@@ -76,7 +85,15 @@ function toggleComplete(task) {
   task.complete = !task.complete;
   // 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 
   // BONUS Task 3: send a fetch request to update the task in json-server so that the complete status will be persisted to db.json
-  renderTask(task);
+  fetch(`http://localhost:3000/tasks/${task.id}`, {
+    method: 'PATCH', 
+    headers: {
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({complete: task.complete})
+  })
+    .then(response => response.json())
+    .then(updateTask)
   return task;
 }
 
@@ -86,12 +103,17 @@ function toggleComplete(task) {
 // Task 1: add an event listener for DOMContentLoaded that will `fetch` the tasks from the json-server (`http://localhost:3000/tasks`)
 //  - parse the body of the response from JSON to an array of todolist tasks
 //  - invoke `loadTodoList`, passing `todoList` as an argument
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/tasks')
+    .then(res => res.json())
+    .then(loadTodoList)
+})
 
 function handleNewTaskSubmit(event) {
   event.preventDefault();
   const label = event.target.labelInput.value;
-  const dueDate = new Date(event.target.dueDateInput.value);
-  addTask(todoList, label, dueDate);
+  const dueDate = event.target.dueDateInput.value;
+  addTask(label, dueDate);
   event.target.reset();
 }
 
